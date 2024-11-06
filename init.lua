07b1db34699d364fcaf532346a17c92ab85663c4
PokeyLink227 <thomasrice7@yahoo.com>
2024-11-05 23:11:35 -0500

    fixed move selection keymap

--- init.lua
+++ init.lua
@@ -169,7 +169,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 -- Move blocks of text in visual mode
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- undotree keybinds
 vim.keymap.set('n', '<leader>u', '<cmd>Telescope undo<cr>')
