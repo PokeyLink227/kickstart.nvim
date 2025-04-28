1fb823d02f364d0b4b28c78242e7d649885ad43a
PokeyLink227 <thomasrice7@yahoo.com>
2025-04-27 23:59:24 -0400

    removed bg change becuase too inconsistent

--- init.lua
+++ init.lua
@@ -994,4 +994,6 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-vim.cmd 'highlight Normal ctermbg=NONE guibg=NONE'
+-- vim.cmd 'highlight Normal ctermbg=NONE guibg=NONE'
+-- vim.cmd 'highlight NormalFloat ctermbg=NONE guibg=NONE'
+-- vim.cmd 'highlight FloatBorder ctermbg=NONE guibg=NONE'
